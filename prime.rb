da470ac5204cb8dfb670dd40b0e539f5a35ed3e0
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-22 19:29:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -12,12 +12,12 @@ def prime?(number)
 #   end
 #   number+=1
 
+# # end
+# (2..number).each do |num|
+# if (number/number== 1  && number/1==number)
+#   return true 
+#   else number < 2
+#     false
 # end
-(2..number).each do |num|
- if (number/number== 1  && number/1==number)
-   return true 
-  else number < 2
-    false
-end
-end
+# end
 end
