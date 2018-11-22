3953d92cd8de3a98c2dabf56b4d62f8c7b8558df
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-22 18:17:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,7 @@
 def prime?(number)
 if number  < 2
   return false
-elsif ( number >2 && number/number== 1  && number/1==number)? true : false
- else
-   return false
 end
+( number >2 && number/number== 1  && number/1==number)? true : false
+
 end
