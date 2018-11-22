1e4c445822f0fafa3efc1ea483bd24fa4473fd73
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-22 17:49:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,15 @@
 # Add  code here!
 def prime?(number)
-  # if number/number== 1  && number/1==number
-  #   return true
-  # else
-  #   return false
-  # end
-  while number > 1
-  (2..number).each {|number|
-    (number/number== 1  && number/1==number) ? true : false
-    number++
-  }
-  end 
-end 
+  if number/number== 1  && number/1==number
+    return true
+  else
+    return false
+  end
+  # while number > 1
+#   (2..number).each {|number|
+#     (number/number== 1  && number/1==number) ? true : false
+#     number++
+#   }
+#   end 
+# end 
 end
