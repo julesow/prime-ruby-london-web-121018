92f143da2bfce4a6a96fb58903d17da4c26c8146
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-22 17:21:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 # Add  code here!
 def prime?(number)
-  if number/number== 1  && number/1==number
-    return true
-  else
-    return false
-  end
+  # if number/number== 1  && number/1==number
+  #   return true
+  # else
+  #   return false
+  # end
+  (number/number== 1  && number/1==number) true :false
+  
 end
