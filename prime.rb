b99c59d9779509e55ff855b8fd85c07b7ef60056
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-22 18:23:18 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,5 @@ def prime?(number)
 if number  < 2
   return false
 end
-(number/number== 1  && number/1==number)? true :
-(number/number== 1  && number/1==number)? true : false
+(number/number== 1  && number/1==number)? true : number
 end
